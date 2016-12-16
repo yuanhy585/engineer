e2dffbe06dd0f423955bcaddab25fb5e56b8719c
yuanhy585 <1269971258@qq.com>
2016-12-16 21:17:36 +0800

    solve the login problem,pay attention to the login type -email

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
                             <label for="email" class="col-md-4 control-label">用户名</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
+                                <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}">
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
