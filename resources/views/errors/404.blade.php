9a60e15e3715f450b0b28251c8fa954107e3f843
yuanhy585 <1269971258@qq.com>
2016-12-20 16:09:12 +0800

    a view for 404 error (route does not exist)

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>NotFoundHttpException</title>
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/blue.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+    <style>
+        body{
+            padding:50px;
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            font-size: 20px;
+            text-align: center;
+        }
+    </style>
+    <script src="/js/jquery.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="panel panel-primary">
+            Whoops!
+            There is something wrong with the URL.
+        </div>
+    </div>
+</div>
+</body>
+</html>
